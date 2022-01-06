270458d67c73f87e05c323a2ee8305af6b866089
Schueder <73704175+Schueder@users.noreply.github.com>
2022-01-06 21:21:37 +0100

    first commit
    add py-script of webServer

---
+++ KeContactHmiPy/webserver.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class GP(BaseHTTPRequestHandler):
+    
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_GET(self):
+        global current
+           
+    
+def run(server_class=HTTPServer, handler_class=GP, port=8088):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    
+    httpd.serve_forever()
+
+
+run()
